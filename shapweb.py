7d7015b108bc8490de6920a11156e1cfdb4b28db
YOKO779 <1099513362@qq.com>
2024-12-21 21:29:21 +0800

    Add files via upload

--- shapweb.py
+++ shapweb.py
@@ -1,17 +1,6 @@
-import streamlit as st
-import shap
-import joblib
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 def main():
     # 加载模型
     lgbm = joblib.load('xgb_model.pkl')  # 更新模型路径
-    # lgbm = joblib.load('./lgbm.pkl')  # 上传到 GitHub 所需路径，路径无需更改
-
-    # 定义输入特征
 
     # 定义特征输入类
     class Subject:
@@ -26,6 +15,11 @@ def main():
             self.糖尿病肾病 = 糖尿病肾病
 
         def make_predict(self):
+            # Check for missing values (for example)
+            if not all([self.认知障碍, self.体育锻炼运动量, self.慢性疼痛, self.营养状态, self.HbA1c, self.查尔斯共病指数, self.步速下降, self.糖尿病肾病]):
+                st.error("Please fill in all the fields")
+                return
+            
             # 将输入数据转化为 DataFrame
             subject_data = {
                 "认知障碍": [self.认知障碍],
@@ -58,22 +52,16 @@ def main():
             # 绘制 SHAP 力图
             shap.force_plot(explainer.expected_value[1], shap_values[1][0, :], df_subject.iloc[0, :], matplotlib=True)
             st.pyplot(plt.gcf())
-            # 瀑布图
-            # ex = shap.Explanation(shap_values[1][0, :], explainer.expected_value[1], df_subject.iloc[0, :])
-            # shap.waterfall_plot(ex)
-            st.pyplot(plt.gcf())
 
-    # 设置页面配置
+    # 页面配置和UI
     st.set_page_config(page_title='老年糖尿病患者衰弱风险预测')
 
-    # 页面标题
     st.markdown(f"""
                 <div class='all'>
                     <h1 style='text-align: center;'>老年糖尿病患者衰弱风险预测</h1>
                 </div>
                 """, unsafe_allow_html=True)
 
-    # 输入特征
     认知障碍 = st.selectbox("认知障碍 (是 = 1, 否 = 0)", [1, 0], index=1)
     体育锻炼运动量 = st.selectbox("体育锻炼运动量 (低运动量 = 1, 中运动量 = 2, 高运动量 = 3)", [1, 2, 3], index=0)
     慢性疼痛 = st.selectbox("慢性疼痛 (有 = 1, 无 = 0)", [1, 0], index=1)
@@ -83,11 +71,8 @@ def main():
     步速下降 = st.selectbox("步速下降 (是 = 1, 否 = 0)", [1, 0], index=1)
     糖尿病肾病 = st.selectbox("糖尿病肾病 (有 = 1, 无 = 0)", [1, 0], index=1)
 
-    # 提交按钮
     if st.button(label="提交"):
         user = Subject(认知障碍, 体育锻炼运动量, 慢性疼痛, 营养状态, HbA1c, 查尔斯共病指数, 步速下降, 糖尿病肾病)
         user.make_predict()
-
-
 
 main()
